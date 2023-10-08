136ad3ef32ae8ace85caff757bcc0f72b061294e
NwabisaMawu <nwabisa.mawu@gmail.com>
2023-10-08 21:54:04 +0200

    made product service manual

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -15,13 +15,62 @@ export class ProductService {
   // to provide the url that will be used to make the http request -  the place where our data is stored
   private productUrl = '/src/api/products/products.json'
 
-  // to provide an instance of the HttpClient service - idnetify it as dependency in the constructor
-  constructor(private http: HttpClient) {
-
-  }
+  // injecting a service - to provide an instance of the HttpClient service - idnetify it as dependency in the constructor
+  constructor(private http: HttpClient) { }
 
   // method that returns an array for products
   getProducts(): IProduct[] {
-    return this.http.get<IProduct[]>(this.productUrl);
+    return [
+      {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      },
+      {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2021",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "assets/images/garden_cart.png"
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2021",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "assets/images/hammer.png"
+      },
+      {
+        "productId": 8,
+        "productName": "Saw",
+        "productCode": "TBX-0022",
+        "releaseDate": "May 15, 2021",
+        "description": "15-inch steel blade hand saw",
+        "price": 11.55,
+        "starRating": 3.7,
+        "imageUrl": "assets/images/saw.png"
+      },
+      {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2020",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "assets/images/xbox-controller.png"
+      }
+    ]
   };
 }
